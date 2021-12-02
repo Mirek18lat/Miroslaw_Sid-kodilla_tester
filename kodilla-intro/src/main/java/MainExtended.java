4c5cbf4757303c853176ade70b9cd40b30c7f6b9
Mirek_Si <mirek18lat@yahoo.fr>
2021-12-02 23:31:32 +0100

    MainExtend

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String country = "Polska";
+        String city = "Warsaw";
+        String street = "Aleje Jerozolimskie";
+        int streetNumber = 22;
+        int apartment = 305;
+        String nameSurname = "Miroslaw Sid";
+        char sex = 'm';
+        System.out.println(country);
+        System.out.println(city);
+        System.out.println(street);
+        System.out.println(streetNumber);
+        System.out.println(apartment);
+        System.out.println(nameSurname);
+        System.out.println(sex);
+    }
+}
