5a0a5888588b5be0a044ae4b62f1403a6eb65d8f
Mirek_Si <mirek18lat@yahoo.fr>
2021-12-21 00:41:35 +0100

    User

---
+++ kodilla-intro/src/main/java/User.java
@@ -0,0 +1,13 @@
+public class User {
+    public static void main(String[] arg) {
+        UserM mirek = new UserM("Mirek", 30);
+        UserM maciek = new UserM("Maciek", 33);
+        UserM michal = new UserM("Michał", 35);
+        UserM agnieszka = new UserM("Agnieszka", 30);
+        UserM apatia = new UserM("Apatia", 33);
+        UserM anna = new UserM("Anna", 35);
+        UserM[] users = {mirek, maciek, michal, agnieszka, apatia, anna};
+        }
+
+
+}
---
+++ kodilla-intro/src/main/java/UserM.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class UserM {
+    String name;
+    int age;
+    int i;
+    int numbers;
+    public UserM (String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public void sum (int[] users) {
+        int sumage = 0;
+        int i = 0;
+        for (i = 0; 0<users.length; i++ ) {
+            sumage += age;
+        }
+       System.out.println(sumage/ users.length);
+    }
+ }
