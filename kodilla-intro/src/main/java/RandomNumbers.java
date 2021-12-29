91408cb4fa6b74e10067aeec661144708139f8d3
Mirek_Si <mirek18lat@yahoo.fr>
2021-12-30 00:36:17 +0100

    29.12.2

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -5,10 +5,21 @@ public class RandomNumbers {
         Random random = new Random();
         int result = 0;
         int sum = 0;
+        int max2 = 0;
+        int min = 31;
         while (sum < 5000) {
             int temp = random.nextInt(31);
             sum = sum + temp;
             result++;
+            if (temp > max2) {
+                max2 = max2;
+            } else
+            if (temp < min) {
+                min = temp;
+            } else {
+                min = min;
+            }
         }
+    return result;
     }
 }
