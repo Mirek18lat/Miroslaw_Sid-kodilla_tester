8b50c0a31b411d9d05520575b9c8aa79d2229ee9
unknown <mirek18lat@yahoo.fr>
2022-01-06 12:58:21 +0100

    06.01_Book

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -2,8 +2,9 @@ public class Book {
     private String author;
     private String title;
 
+
     public static String of(String author, String title) {
         String book = Book.of("Isaac Asimov", "The Galaxy");
-        return book;
+        return new Book(author, title);
     }
 }
