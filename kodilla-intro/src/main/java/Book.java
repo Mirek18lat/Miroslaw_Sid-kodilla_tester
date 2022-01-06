24cdb879f2a52233134351ce3b10c28b52b69579
unknown <mirek18lat@yahoo.fr>
2022-01-06 11:48:53 +0100

    06.01_Book

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -1,14 +1,9 @@
-import java.util.Scanner;
-
 public class Book {
     private String author;
     private String title;
-    public static String of(String author, String title) {
-        return author + title;
-    }
 
-    public static void main(String[] args) {
-        String book = Book.of("Isaac Asimov", "The Galaxy" );
-        System.out.println(book);
+    public static String of(String author, String title) {
+        String book = Book.of("Isaac Asimov", "The Galaxy");
+        return book;
     }
 }
