e36139c6a94ef81fe75bd579d619aa4a7383092b
Mirek_Si <mirek18lat@yahoo.fr>
2022-03-31 22:58:09 +0200

    31.03_9.3_CarConfiguration

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarConfiguration.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarConfiguration.java
@@ -11,15 +11,40 @@ import java.util.Date;
 @Configuration
 public class CarConfiguration {
     Car car;
+    String season;
     @Bean
     public Car chooseCar () {
 
-        if (LocalDateTime.now().getMonth() == Month.MARCH || LocalDateTime.now().getMonth() == Month.APRIL || LocalDateTime.now().getMonth() == Month.MAY || LocalDateTime.now().getMonth() == Month.SEPTEMBER || LocalDateTime.now().getMonth() == Month.OCTOBER || LocalDateTime.now().getMonth() == Month.NOVEMBER) {
+        if (isSpringSeason() || isAutumnSeason()) {
             car = new Sedan();
-        } else if (LocalDateTime.now().getMonth() == Month.JUNE || LocalDateTime.now().getMonth() == Month.JULY || LocalDateTime.now().getMonth() == Month.AUGUST) {
+        } else if (isSummerSeason()) {
             car = new Cabrio();
         } else {car = new SUV();}
         return car;
+    }
+
+    public boolean isSpringSeason() {
+        if (LocalDateTime.now().getMonth() == Month.MARCH || LocalDateTime.now().getMonth() == Month.APRIL || LocalDateTime.now().getMonth() == Month.MAY ) {
+            return true;
+        } else {return false;}
+    }
+
+    public boolean isSummerSeason() {
+        if (LocalDateTime.now().getMonth() == Month.JUNE || LocalDateTime.now().getMonth() == Month.JULY || LocalDateTime.now().getMonth() == Month.AUGUST) {
+            return true;
+        } else {return false;}
+    }
+
+    public boolean isAutumnSeason() {
+        if (LocalDateTime.now().getMonth() == Month.SEPTEMBER || LocalDateTime.now().getMonth() == Month.OCTOBER || LocalDateTime.now().getMonth() == Month.NOVEMBER) {
+            return true;
+        } else {return false;}
+    }
+
+    public boolean isWinterSeason() {
+        if (LocalDateTime.now().getMonth() == Month.DECEMBER || LocalDateTime.now().getMonth() == Month.JANUARY || LocalDateTime.now().getMonth() == Month.FEBRUARY) {
+            return true;
+        } else {return false;}
     }
 
 }
